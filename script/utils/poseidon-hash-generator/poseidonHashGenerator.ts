b6afe8927ee2019bddb7e096b6bba6dd61b44433
masaun <masaun@protonmail.com>
2025-03-24 21:16:33 +0900

    [SC script]: Add the execution of the poseidonHash() -> In Progress

--- script/utils/poseidon-hash-generator/poseidonHashGenerator.ts
+++ script/utils/poseidon-hash-generator/poseidonHashGenerator.ts
@@ -2,11 +2,13 @@ import { BackendInstances, Circuits, Noirs } from './types.js';
 import { join, resolve } from 'path';
 
 import { Noir, type CompiledCircuit, type InputMap } from '@noir-lang/noir_js';
+import { UltraHonkBackend } from '@aztec/bb.js';
 import { BarretenbergBackend } from '@noir-lang/backend_barretenberg';
 import { compile, createFileManager } from '@noir-lang/noir_wasm';
 import { ProofData } from '@noir-lang/types';
 
-let circuitArtifact: any;
+
+//let circuitArtifact: any;
 
 // /**
 //  * @notice - Get the circuit from the specified path
@@ -22,28 +24,62 @@ let circuitArtifact: any;
 //   return compiled.program;
 // }
 
+
 /** 
  * @notice - Get the circuit artifact (= Compiled Circuit) from the specified path
  */
-async function getCircuitArtifact() {
-  circuitArtifact = await import(`../../../circuits/target/ip_nft_ownership.json`);
-  return circuitArtifact;
+async function getCircuitArtifact(): Promise<CompiledCircuit> {
+  let circuitArtifact = await import(`../../../circuits/target/ip_nft_ownership.json`);
+  return circuitArtifact as CompiledCircuit;
 }
 
 /** 
  * @notice - Get the poseidon hash from the specified data
  */
 async function poseidonHash(data1: any, data2: any, data3: any): Promise<string> {
-    const circuitArtifact = await getCircuitArtifact();
-    const backend = new BarretenbergBackend(circuitArtifact.bytecode);
+    let circuitArtifact: CompiledCircuit = await getCircuitArtifact();
+    //console.log(`circuitArtifact: ${JSON.stringify(circuitArtifact)}`);
+    //console.log(`circuitArtifact.bytecode: ${JSON.stringify(circuitArtifact.bytecode)}`);  /// [Result]: Success to retrieve in String.
+    if (!circuitArtifact || !circuitArtifact.bytecode) {
+      throw new Error('Circuit artifact or bytecode is undefined');
+    }
+
+
+    const backend = new UltraHonkBackend(circuitArtifact.bytecode);
+    //const backend = new BarretenbergBackend(circuitArtifact);
+    //const backend = new BarretenbergBackend(circuitArtifact.bytecode, { threads: 8 });
+    //const noir = new Noir(circuitArtifact as CompiledCircuit);
     const noir = new Noir(circuitArtifact as CompiledCircuit);
   
-    const hashPrivate = await noir.execute({
-    //const hashPrivate = await noirPoseidon.execute({
-      amount1: data1.toString(),
-      amount2: data2.toString(),
-      secretShare: data3.toString(),
-    });
+    const inputs: InputMap = {
+      //const hashPrivate = await noirPoseidon.execute({
+        amount1: data1.toString(),
+        amount2: data2.toString(),
+        secretShare: data3.toString(),
+      }
+
+    const { witness } = await noir.execute(inputs);
+    // const hashPrivate = await noir.execute({
+    //   amount1: data1.toString(),
+    //   amount2: data2.toString(),
+    //   secretShare: data3.toString(),
+    // });
   
-    return hashPrivate.returnValue.toString();
+    return witness.toString();
+    //return hashPrivate.returnValue.toString();
 }
+
+
+/**
+ * @notice - Main function
+ */
+async function main(): Promise<string> {
+  const data1 = 100;
+  const data2 = 200;
+  const data3 = 300;
+  const hashValueInPoseidon = await poseidonHash(data1, data2, data3);
+  console.log(`hashValueInPoseidon: ${hashValueInPoseidon}`);
+  return hashValueInPoseidon;
+}
+
+// const a = main();
