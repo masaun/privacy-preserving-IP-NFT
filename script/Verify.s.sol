1209140babbe0cb28540d81d2fbe90e7f0cf2835
masaun <masaun@protonmail.com>
2025-03-27 11:58:25 +0900

    [SC script]: Using the vm.ffi() to run the poseidon2HashGenerator.ts - when running the Verify.s.sol

--- script/Verify.s.sol
+++ script/Verify.s.sol
@@ -8,7 +8,7 @@ import { UltraVerifier } from "../../contracts/circuit/plonk_vk.sol";
 //import { UltraVerifier } from "../../circuits/target/contract.sol";
 import { ProofConverter } from "./utils/ProofConverter.sol";
 
-import { PoseidonUnit5L } from "iden3-contracts/lib/Poseidon.sol"; /// @dev - iden3/contracts/contracts/lib/Poseidon.sol --> [NOTE]: Import path has been adjusted. See the remapping.txt
+//import { PoseidonUnit5L } from "iden3-contracts/lib/Poseidon.sol"; /// @dev - iden3/contracts/contracts/lib/Poseidon.sol --> [NOTE]: Import path has been adjusted. See the remapping.txt
 
 contract VerifyScript is Script {
     IPNFTOwnershipVerifier public ipNFTOwnershipVerifier;
@@ -18,7 +18,7 @@ contract VerifyScript is Script {
 
     function run() public returns (bool) {
         // @dev - Test
-        uint256 resultInPoseidonHash = computePoseidonHash([uint256(1), uint256(2), uint256(3), uint256(4), uint256(5)]);
+        uint256 resultInPoseidonHash = computePoseidon2Hash([uint256(1), uint256(2), uint256(3), uint256(4), uint256(5)]);
         console.logUint(resultInPoseidonHash);
 
 
@@ -42,8 +42,15 @@ contract VerifyScript is Script {
     }
 
 
-    function computePoseidonHash(uint256[5] memory inputs) public pure returns (uint256) {
-        return PoseidonUnit5L.poseidon(inputs); // PoseidonUnit5L for 5 inputs
+    function computePoseidon2Hash(uint256[5] memory inputs) public returns (uint256) {
+        // Generate the proof by calling the script using ffi
+        string[] memory ffi_command = new string[] (2);
+        ffi_command[0] = "./script/utils/poseidon2-hash-generator/usages/sync/runningScript_poseidon2HashGenerator.sh";
+        //ffi_command[1] = _testName;
+        bytes memory commandResponse = vm.ffi(ffi_command);
+
+        //return PoseidonUnit5L.poseidon(inputs); // PoseidonUnit5L for 5 inputs
+        return 1; // Temporary
     }
 
 }
