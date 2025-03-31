2dd953b945af3262ff230e64bee202a5448b41d9
masaun <masaun@protonmail.com>
2025-03-31 11:30:46 +0900

    [SC script]: Switch the position of 2 properties ('merkleRoot' and 'nullifier') in the Poseidon2HashAndPublicInputs --> Successful that the each values are stored into the corresponded-properties

--- script/Verify.s.sol
+++ script/Verify.s.sol
@@ -16,8 +16,8 @@ contract VerifyScript is Script {
 
     struct Poseidon2HashAndPublicInputs {
         string hash;
-        bytes32 nullifier;
         bytes32 merkleRoot;
+        bytes32 nullifier;
     }
 
     function setUp() public {}
@@ -26,7 +26,6 @@ contract VerifyScript is Script {
         verifier = new UltraVerifier();
         ipNFTOwnershipVerifier = new IPNFTOwnershipVerifier(verifier);
 
-
         // @dev - Retrieve the Poseidon2 hash and public inputs, which was read from the output.json file
         Poseidon2HashAndPublicInputs memory poseidon2HashAndPublicInputs = computePoseidon2Hash();
         bytes32 merkleRoot = poseidon2HashAndPublicInputs.merkleRoot;   // [Log]: 0x215597bacd9c7e977dfc170f320074155de974be494579d2586e5b268fa3b629
@@ -34,8 +33,8 @@ contract VerifyScript is Script {
         bytes32 nullifierHash = poseidon2HashAndPublicInputs.nullifier; // [Log]: 0x168758332d5b3e2d13be8048c8011b454590e06c44bce7f702f09103eef5a373
         //bytes32 nullifierHash = 0x168758332d5b3e2d13be8048c8011b454590e06c44bce7f702f09103eef5a373; // [Result]: Successful (NOTE: This is equal to Field(10190015755989328289879378487807721086446093622177241109507523918927702106995) in Noir)
         //bytes32 nullifierHash = 10190015755989328289879378487807721086446093622177241109507523918927702106995;
-        console.logBytes32(merkleRoot);
-        console.logBytes32(nullifierHash);
+        console.logBytes32(merkleRoot);     // [Log]: 0x215597bacd9c7e977dfc170f320074155de974be494579d2586e5b268fa3b629
+        console.logBytes32(nullifierHash);  // [Log]: 0x168758332d5b3e2d13be8048c8011b454590e06c44bce7f702f09103eef5a373
 
         bytes memory proof_w_inputs = vm.readFileBinary("./circuits/target/ip_nft_ownership_proof.bin");
         bytes memory proofBytes = ProofConverter.sliceAfter64Bytes(proof_w_inputs);
@@ -72,13 +71,13 @@ contract VerifyScript is Script {
 
         /// @dev - Read the output.json file and parse the JSON data
         string memory json = vm.readFile("script/utils/poseidon2-hash-generator/usages/async/output/output.json");
+        console.log(json);
         bytes memory data = vm.parseJson(json);
         Poseidon2HashAndPublicInputs memory poseidon2HashAndPublicInputs = abi.decode(data, (Poseidon2HashAndPublicInputs));
-        console.log(json);
         //console.logBytes(data);
         console.logString(poseidon2HashAndPublicInputs.hash);
-        console.logBytes32(poseidon2HashAndPublicInputs.nullifier);
         console.logBytes32(poseidon2HashAndPublicInputs.merkleRoot);
+        console.logBytes32(poseidon2HashAndPublicInputs.nullifier);
 
         return poseidon2HashAndPublicInputs;
     }
