56accbf9a624bba5133c88c87ffcfba8713a0e45
masaun <masaun@protonmail.com>
2025-04-02 17:39:35 +0900

    [SC test]: Implement the test_verifyProof() and the test_wrongProof() in the IPNFTOwnershipVerifier.t.sol -> Successful to pass both tests

--- test/IPNFTOwnershipVerifier.t.sol
+++ test/IPNFTOwnershipVerifier.t.sol
@@ -21,17 +21,61 @@ contract IPNFTOwnershipVerifierTest is Test {
     }
 
     function test_verifyProof() public {
-        noirHelper.withInput("x", 1).withInput("y", 1).withInput("return", 1);
+        uint256[] memory hash_path = new uint256[](2);
+        hash_path[0] = 0x1efa9d6bb4dfdf86063cc77efdec90eb9262079230f1898049efad264835b6c8;
+        hash_path[1] = 0x2a653551d87767c545a2a11b29f0581a392b4e177a87c8e3eb425c51a26a8c77;
+
+        bytes32[] memory hash_path_bytes32 = new bytes32[](2);
+        hash_path_bytes32[0] = bytes32(hash_path[0]);
+        hash_path_bytes32[1] = bytes32(hash_path[1]);
+
+        /// @dev - Set the input data for generating a proof
+        noirHelper.withInput("root", bytes32(uint256(0x215597bacd9c7e977dfc170f320074155de974be494579d2586e5b268fa3b629)))
+                  .withInput("hash_path", hash_path_bytes32)
+                  .withInput("index", bytes32(uint256(0)))
+                  .withInput("secret",bytes32(uint256(1)))                   
+                  .withInput("expected_nullifier", bytes32(uint256(0x1265c921cb8e0dc6c91f70ae08b14352b8f10451aee7582b9ed44abea8d4123c)))
+                  .withStruct("ip_nft_data")
+                  .withStructInput("nft_owner", bytes32(uint256(uint160(0xC6093Fd9cc143F9f058938868b2df2daF9A91d28)))) // [NOTE]: An input data of 'Address' type must be cast to uint160 first. Then, it should be cast to uint256 and bytes32.
+                  .withStructInput("nft_token_id", bytes32(uint256(1)))
+                  .withStructInput("metadata_cid_hash", bytes32(uint256(0x1efa9d6bb4dfdf86063cc77efdec90eb9262079230f1898049efad264835b6c8)));
+
+        /// @dev - Generate the proof
         (bytes32[] memory publicInputs, bytes memory proof) = noirHelper.generateProof("test_verifyProof", 2);
         ipNFTOwnershipVerifier.verifyIPNFTOwnership(proof, publicInputs);
     }
 
     function test_wrongProof() public {
         noirHelper.clean();
-        noirHelper.withInput("x", 1).withInput("y", 5).withInput("return", 5);
+        uint256[] memory hash_path = new uint256[](2);
+        hash_path[0] = 0x1efa9d6bb4dfdf86063cc77efdec90eb9262079230f1898049efad264835b6c8;
+        hash_path[1] = 0x2a653551d87767c545a2a11b29f0581a392b4e177a87c8e3eb425c51a26a8c77;
+
+        bytes32[] memory hash_path_bytes32 = new bytes32[](2);
+        hash_path_bytes32[0] = bytes32(hash_path[0]);
+        hash_path_bytes32[1] = bytes32(hash_path[1]);
+
+        /// @dev - Set the input data for generating a proof
+        noirHelper.withInput("root", bytes32(uint256(0x215597bacd9c7e977dfc170f320074155de974be494579d2586e5b268fa3b629)))
+                  .withInput("hash_path", hash_path_bytes32)
+                  .withInput("index", bytes32(uint256(0)))
+                  .withInput("secret",bytes32(uint256(1)))                   
+                  .withInput("expected_nullifier", bytes32(uint256(0x1265c921cb8e0dc6c91f70ae08b14352b8f10451aee7582b9ed44abea8d4123c)))
+                  .withStruct("ip_nft_data")
+                  .withStructInput("nft_owner", bytes32(uint256(uint160(0xC6093Fd9cc143F9f058938868b2df2daF9A91d28)))) // [NOTE]: An input data of 'Address' type must be cast to uint160 first. Then, it should be cast to uint256 and bytes32.
+                  .withStructInput("nft_token_id", bytes32(uint256(1)))
+                  .withStructInput("metadata_cid_hash", bytes32(uint256(0x1efa9d6bb4dfdf86063cc77efdec90eb9262079230f1898049efad264835b6c8)));
+
+        /// @dev - Generate the proof
         (bytes32[] memory publicInputs, bytes memory proof) = noirHelper.generateProof("test_wrongProof", 2);
+
+        /// @dev - Create a fake public input, which should fail because the public input is wrong
+        bytes32[] memory fakePublicInputs = new bytes32[](2);
+        fakePublicInputs[0] = publicInputs[0];
+        fakePublicInputs[1] = bytes32(uint256(0xddddd));  // @dev - This is wrong publicInput ("nulifieir")
+
         vm.expectRevert();
-        ipNFTOwnershipVerifier.verifyIPNFTOwnership(proof, publicInputs);
+        ipNFTOwnershipVerifier.verifyIPNFTOwnership(proof, fakePublicInputs);
     }
 
     // function test_all() public {
