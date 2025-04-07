7ac2768df4df4be049035968309b2f4f9d741cf4
masaun <masaun@protonmail.com>
2025-04-07 11:23:30 +0900

    [SC script - deployment]: Modify the DeploymentAllContracts.s.sol and deploymentScript_AllContracts.sh -> Error: Multi chain deployment does not support library linking at the moment

--- script/edu-chain-testnet/deployment/deploymentScript_AllContracts.sh
+++ script/edu-chain-testnet/deployment/deploymentScript_AllContracts.sh
@@ -5,4 +5,5 @@ echo "Deploying the UltraVerifier and the IPNFTOwnershipVerifier contract on EDU
 forge script script/edu-chain-testnet/deployment/DeploymentAllContracts.s.sol --broadcast --private-key ${EDU_CHAIN_TESTNET_PRIVATE_KEY} \
     ./contracts/circuit/plonk_vk.sol:UltraVerifier \
     ./IPNFTOwnershipVerifier.sol:IPNFTOwnershipVerifier \
+    ./IPNFTFactory.sol:IPNFTFactory \
     --skip-simulation
